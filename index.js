956356da9a90eb63b01c955271cdb564a70b0dda
Nayem Ahmed <nayemk0087@gmail.com>
2026-05-19 15:09:35 +0600

    create database and store collection in my personal server

--- index.js
+++ index.js
@@ -18,13 +18,22 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db('pawnest')
+    const petCollection = db.collection('pets')
+
+    app.post('/pet', async (req, res) => {
+        const petData = req.body
+        const result = await petCollection.insertOne(petData)
+
+        res.json(result)
+    })
+
+  
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
